698b2a8d660fd853a0fb00a45c260bb50b64b7f5
cfriderici <christina@friderici.net>
2022-09-27 21:52:56 +0200

    lesson 27.09.22

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,28 +5,42 @@ import { FaRegCheckSquare } from "react-icons/fa";
 
 
 //useRef-Hook für Datenabfrage einfügen
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 
 
-const Todo = ({title}) => {
+// state hook muss hier auch definiert werden
+const Todo = ({title, todos, setTodos}) => {
 
     // den Hoock in einer const definieren
     const todoDeleteRef = useRef();
+    const todoUndoneRef = useRef();
+    const todoDoneRef = useRef();
+
+
 
     // onKlick-Funktion definieren --> sie muss im selben element wie der useRef sein
-    const handleDeleteClick = () => {
-        console.log(todoDeleteRef.current.children[2]);
+    const handleUndoneClick = () => {
+        console.log(todoUndoneRef.current.children[0]);
+        // console.log(todoDeleteRef.current);
     }
+    const handleDoneClick = () => {
+        console.log(todoDoneRef.current.children[0]);
+    }
+    const handleDeleteClick = () => {
+        console.log(todoDeleteRef.current.children[0]);
+    }
+
+
     
 
     return (
         <div className="todo">
             {title} 
-            <div className="img-wrapper" ref={todoDeleteRef}>
-                <FaRegSquare className="todo-undone" />
-                <FaRegCheckSquare className="todo-done" />
-                <FaRegTrashAlt className="input-delete" onClick={handleDeleteClick} />
+            <div className="img-wrapper"  >
+                <span className="undone" ref={todoUndoneRef}><FaRegSquare className="todo-undone" onClick={handleUndoneClick}/></span>
+                <span className="done" ref={todoDoneRef}><FaRegCheckSquare className="todo-done" onClick={handleDoneClick}/></span>
+                <span className="delete>"  ref={todoDeleteRef}><FaRegTrashAlt className="input-delete" onClick={handleDeleteClick}/></span>
             </div> 
         </div>
     );
